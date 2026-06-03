9c275cbc9da82c08b1f6064e438072f0d2df6645
Gayatri <gayatrisingh44567@gmail.com>
2026-06-03 15:41:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function generatePassword() {
+    const chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+
+    let password = "";
+
+    for (let i = 0; i < 12; i++) {
+        const randomIndex =
+            Math.floor(Math.random() * chars.length);
+
+        password += chars[randomIndex];
+    }
+
+    document.getElementById("password").value = password;
+    
+    const strength = checkStrength(password);
+    document.getElementById("strength").innerText =
+
+        "Strength: " + strength;
+
+}
+
+function checkStrength(password) {
+    if (password.length < 8) {
+        return "Weak";
+    } else if (password.length < 12) {
+        return "Medium";
+    } else {
+        return "Strong";
+    }
+}
+
+function copyPassword() {
+    const passwordField =
+        document.getElementById("password");
+
+    navigator.clipboard.writeText(passwordField.value);
+
+    alert("Password copied!");
+}
